a0b3525c38dc5c0978ee48cb4c64b553338bb5d0
Steven Nkeneng <47323934+nkeneng@users.noreply.github.com>
2025-06-12 18:49:41 +0200

    feat(widget): replace card list with carousel

---
+++ app/javascript/shared/components/ChatCardCarousel.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { Splide, SplideSlide } from '@splidejs/vue-splide';
+import { toRefs } from 'vue';
+import '@splidejs/vue-splide/css';
+import ChatCard from './ChatCard.vue';
+
+const props = defineProps({
+  items: { type: Array, default: () => [] },
+});
+const { items } = toRefs(props);
+</script>
+
+<template>
+  <Splide :options="{ pagination: false, arrows: true }" class="w-full">
+    <SplideSlide v-for="item in items" :key="item.title">
+      <ChatCard
+        :media-url="item.media_url"
+        :title="item.title"
+        :description="item.description"
+        :actions="item.actions"
+        class="mx-1"
+      />
+    </SplideSlide>
+  </Splide>
+</template>
